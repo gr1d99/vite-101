b0aef4f258de3b46883c4028c44ae63e87998d22
Gideon Kimutai <gideon.kimutai.kim@gmail.com>
2023-03-23 10:30:01 +0300

    chore: add tailwind

---
+++ src/app.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = (): JSX.Element => {
+  return (
+    <div className='pt-4'>
+        <h1 className='text-center font-bold text-lg'>Weather</h1>
+    </div>
+  )
+}
+
+export default App
